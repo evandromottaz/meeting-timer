94f535ca3511c99a90f3be661076ff9ecb3364e5
evandromottaz <evandro.motta@live.com>
2023-06-15 09:03:25 -0300

    mapper created

--- src/constants/schedule.ts
+++ src/constants/schedule.ts
@@ -1,18 +1,38 @@
+import { FormValueType, FormValuesProps } from "../types/meetingSchedule";
+
 const name = { name: "" };
 const time = { time: "" };
 const advice = { advice: "" };
 export const values = { ...name, ...time };
-export const ministryField = { ...values, ...advice };
+export const ministryField = { ...values, ...advice, placeholder: "" };
 
-export const meetingSchedule = {
-	president: "",
-	initialComments: "",
-	treasures: [
-		{ placeholder: "Orador", ...values },
-		{ placeholder: "Dirigente", ...values },
+const KEYS: [keyof FormValuesProps, FormValueType][] = [
+	["president", { pdfText: "Presidente", placeholder: "Nome", ...name }],
+	["initialComments", { pdfText: "Comentários Iniciais", ...values }],
+	[
+		"treasures",
+		{
+			primary: {
+				pdfText: "Tesouros da Palavra de Deus",
+				placeholder: "Orador",
+				...values,
+			},
+			secondary: {
+				pdfText: "Encontre Joias Espirituais",
+				placeholder: "Dirigente",
+				...values,
+			},
+		},
 	],
-	ministryField: [ministryField],
-	christianLife: [values],
-	bibleStudy: values,
-	finalComments: "",
-};
+	[
+		"ministryField",
+		{
+			pdfText: "Faça seu melhor no ministério",
+			fields: [{ ...ministryField }],
+		},
+	],
+	["bibleStudy", { pdfText: "Estudo Bíblico", ...values }],
+	["finalComments", { pdfText: "Comentários Finais", ...values }],
+];
+
+export const keyMap = new Map(KEYS);
--- src/types/meetingSchedule.ts
+++ src/types/meetingSchedule.ts
@@ -1,18 +1,41 @@
-type time = { time: string };
-type name = { name: string };
-type placeholder = { placeholder: string };
-type advice = { advice: string };
+type placeholderType = { placeholder: string };
+type pdfTextType = { pdfText: string };
+type nameType = { name: string };
+type timeType = { time: string };
 
-type valuesTypes = time & name;
-type treasuresTypes = valuesTypes & placeholder;
-type ministryFieldTypes = valuesTypes & advice;
+interface FormValueProps extends placeholderType, nameType, timeType {}
 
-export interface FormValues {
-	president: string;
-	initialComments: string;
-	treasures: treasuresTypes[];
-	ministryField: ministryFieldTypes[];
-	christianLife: valuesTypes[];
-	bibleStudy: valuesTypes;
-	finalComments: string;
+interface PresidentProps extends pdfTextType, nameType, placeholderType {}
+
+interface CommentsProps extends pdfTextType, nameType, timeType {}
+
+interface ChristianLifeProps extends FormValueProps {}
+
+interface BibleStudyProps extends FormValueProps {}
+
+interface TreasuresProps {
+	primary: FormValueProps & pdfTextType;
+	secondary: FormValueProps & pdfTextType;
 }
+
+interface MinistryFieldProps extends pdfTextType {
+	fields: [FormValueProps];
+}
+
+export interface FormValuesProps {
+	president: PresidentProps;
+	initialComments: CommentsProps;
+	treasures: TreasuresProps;
+	ministryField: MinistryFieldProps;
+	christianLife: ChristianLifeProps[];
+	bibleStudy: BibleStudyProps;
+	finalComments: CommentsProps;
+}
+
+export type FormValueType =
+	| FormValueProps
+	| TreasuresProps
+	| MinistryFieldProps
+	| FormValueProps[]
+	| PresidentProps
+	| CommentsProps;
