1ceefae5f4644adfa6f0ee0d285cb1e336d6918b
evandromottaz <evandro.motta@live.com>
2023-08-07 08:20:48 -0300

    add: types

--- src/types/meetingSchedule.ts
+++ src/types/meetingSchedule.ts
@@ -1,11 +1,11 @@
-type placeholderType = { placeholder: string };
-type pdfTextType = { pdfText: string };
-type nameType = { name: string };
-type timeType = { time: string };
+export type placeholderType = { placeholder: string };
+export type pdfTextType = { pdfText: string };
+export type nameType = { name: string };
+export type timeType = { time: string };
 
 interface FormValueProps extends placeholderType, nameType, timeType {}
 
-interface PresidentProps extends pdfTextType, nameType, placeholderType {}
+interface PresidentProps extends pdfTextType, nameType {}
 
 interface CommentsProps extends pdfTextType, nameType, timeType {}
 
@@ -18,24 +18,14 @@ interface TreasuresProps {
 	secondary: FormValueProps & pdfTextType;
 }
 
-interface MinistryFieldProps extends pdfTextType {
-	fields: [FormValueProps];
-}
+interface MinistryFieldProps extends FormValueProps, pdfTextType {}
 
 export interface FormValuesProps {
 	president: PresidentProps;
 	initialComments: CommentsProps;
 	treasures: TreasuresProps;
-	ministryField: MinistryFieldProps;
+	ministryField: [MinistryFieldProps];
 	christianLife: ChristianLifeProps[];
 	bibleStudy: BibleStudyProps;
 	finalComments: CommentsProps;
 }
-
-export type FormValueType =
-	| FormValueProps
-	| TreasuresProps
-	| MinistryFieldProps
-	| FormValueProps[]
-	| PresidentProps
-	| CommentsProps;
