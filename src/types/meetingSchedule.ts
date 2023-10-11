a87968d0cb8c4f192453de7395fb7b22d0ac885c
Evandro Motta <evandro.motta@live.com>
2023-10-11 09:39:09 -0300

    correção de types

--- src/types/meetingSchedule.ts
+++ src/types/meetingSchedule.ts
@@ -1,31 +1,52 @@
-export type placeholderType = { placeholder: string };
-export type pdfTextType = { pdfText: string };
+export type placeholderType = { placeholder?: string };
+export type pdfTextType = { pdfText?: string };
 export type nameType = { name: string };
 export type timeType = { time: string };
+export type adviceType = { advice?: string }
+export type limitTimeType = { limitTime?: string }
 
-interface FormValueProps extends placeholderType, nameType, timeType {}
+type commonInputType = pdfTextType & timeType & nameType
+type studiesType = commonInputType & placeholderType & adviceType
 
-interface PresidentProps extends pdfTextType, nameType {}
+interface FormValueProps extends placeholderType, nameType, timeType { }
 
-interface CommentsProps extends pdfTextType, nameType, timeType {}
-
-interface ChristianLifeProps extends FormValueProps {}
-
-interface BibleStudyProps extends FormValueProps {}
-
-interface TreasuresProps {
-	primary: FormValueProps & pdfTextType;
-	secondary: FormValueProps & pdfTextType;
+export interface MeetingProps {
+	start: pdfTextType & timeType;
+	end: pdfTextType & timeType
 }
 
-interface MinistryFieldProps extends FormValueProps, pdfTextType {}
+interface PresidentProps extends pdfTextType, nameType { }
+
+interface CommentsProps {
+	initial: pdfTextType & timeType,
+	end: pdfTextType & timeType,
+}
+
+interface TreasuresProps {
+	treasure: commonInputType;
+	findTreasures: commonInputType;
+}
+
+export interface MinistryFieldProps {
+	templates: {
+		video: commonInputType
+		firstMeeting: studiesType
+		secondMeeting: studiesType
+		bibleStudy: studiesType
+	};
+	fields: (FormValueProps & pdfTextType & adviceType)[]
+}
+
+interface ChristianLifeProps {
+	templates: nameType & timeType,
+	fields: (FormValueProps & limitTimeType & pdfTextType)[]
+}
 
 export interface FormValuesProps {
+	meeting: MeetingProps;
 	president: PresidentProps;
-	initialComments: CommentsProps;
+	comments: CommentsProps
 	treasures: TreasuresProps;
-	ministryField: [MinistryFieldProps];
-	christianLife: ChristianLifeProps[];
-	bibleStudy: BibleStudyProps;
-	finalComments: CommentsProps;
+	ministryField: MinistryFieldProps;
+	christianLife: ChristianLifeProps;
 }
