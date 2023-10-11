f796cc489a20bc85e69752a61b56a6c4aeade4d8
Evandro Motta <evandro.motta@live.com>
2023-10-11 09:33:51 -0300

    remove: css

--- src/constants/query.ts
+++ src/constants/query.ts
@@ -18,20 +18,4 @@ export const QUERY = {
 			TIME: "treasures.findTreasures.time",
 		},
 	},
-	MINISTRY_FIELD: {
-		BIBLE_READ: {
-			NAME: "ministryField.bibleRead.name",
-			TIME: "ministryField.bibleRead.time",
-			ADVICE: "ministryField.bibleRead.advice.time",
-		},
-		STUDIES: {
-			NAME: (index: number) => `ministryField.study.${index}.name`,
-			TIME: (index: number) => `ministryField.study.${index}.time`,
-			ADVICE: (index: number) => `ministryField.study.${index}.advice`,
-		},
-	},
-	CHRISTIAN_LIFE: {
-		NAME: (index: number) => `christianLife.${index}.name`,
-		TIME: (index: number) => `christianLife.${index}.time`,
-	},
 };
